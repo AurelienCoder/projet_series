25e13946a11c580cd2de060c4d7520cfea22d6c1
hacker258036 <hassirouibrahim98@gmail.com>
2025-04-28 09:51:56 +0200

    modifications

---
+++ pages/footer.php
@@ -0,0 +1,8 @@
+<footer class="navbar navbar-dark bg-dark">
+    <div class="navbar-brand" style="width:100% ; text-align:center ; font-size: 0.8em">
+            <a href="https://www.allocine.fr/">
+                Source
+            </a>
+        </div>
+    </div>
+</footer>
---
+++ pages/header.php
@@ -0,0 +1,32 @@
+
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="nav-content">
+            <a class="navbar-brand" href="index.php">
+                <div style="width: 0.5em"></div>
+                <h1>Web Series</h1> |
+                <h1>Séries</h1>
+            </a>
+            
+
+            <div style="flex: 1"></div>
+
+            <div class="btn-group" role="group">   
+                <div class="dropdown">
+                    <button class="btn btn-dark  dropdown-toggle" role="button" data-bs-toggle="dropdown">
+                        <span class="caret"></span></button>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+                        <li><a class="dropdown-item" href="pages/logout.php">Logout</a></li>
+                    </div>
+                </div>
+           
+                <a class="btn btn-dark" role="button" href="pages/login.php">
+                    Login
+                </a>
+    
+            </div>
+
+        </div>
+    </nav>
+</header>
